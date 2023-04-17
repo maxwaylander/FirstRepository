e17d7cf346f5297db4749c6006d740da78ed344b
Max Klimik <maxwaylander@mail.ru>
2023-04-17 14:26:47 +0500

    слайд 17 из Урока 5-6

--- index.php
+++ index.php
@@ -138,56 +138,117 @@
 //echo $sum;
 
 //УРОК 5-6
-echo " <br> 1 задание 15 слайда <br>";
-$j = 50;
-$arr3 = [];
-$result = 0;
-for ($i = 0; $i < 5; $i++) {
-    $arr3[$i] = $j - $i * 5;//$arr3[0] = 50 - 0*5 = 50 $arr3[1] = 50-1*5=45
-    echo $arr3[$i] . " ";
-//$result = $j + $arr3[1] + $arr3[2] + $arr3[3] + $arr3[4];
-    $result += $arr3[$i];
-}
-echo $result;
-echo '<br>';
+//echo " <br> 1 задание 15 слайда <br>";
+//$j = 50;
+//$arr3 = [];
+//$result = 0;
+//for ($i = 0; $i < 5; $i++) {
+//    $arr3[$i] = $j - $i * 5;//$arr3[0] = 50 - 0*5 = 50 $arr3[1] = 50-1*5=45
+//    echo $arr3[$i] . " ";
+////$result = $j + $arr3[1] + $arr3[2] + $arr3[3] + $arr3[4];
+//    $result += $arr3[$i];
+//}
+//echo $result;
+//echo '<br>';
+//
+//echo " <br> 2 задание 15 слайда <br>";
+//$arr4 = [];
+//for ($i = 0; $i <= 10; $i++) {
+//    $arr4[$i] = mt_rand(0, 10);
+//    echo $arr4[$i] . " ";
+//}
+//echo '<br>';
+//for ($i = count($arr4) - 1; $i >= 0; $i--) {
+//    echo $arr4[$i] . " ";
+//}
+//echo '<br>';
+//
+//echo " <br> 3 задание 15 слайда <br>";
+//$arr5 = ['1.png', '2.png', '3.png', '4.png'];
+//$d = mt_rand(0, 3);
+//echo $arr5[$d];
+//echo '<br>';
+//
 
-echo " <br> 2 задание 15 слайда <br>";
-$arr4 = [];
-for ($i = 0; $i <= 10; $i++) {
-    $arr4[$i] = mt_rand(0, 10);
-    echo $arr4[$i] . " ";
-}
-echo '<br>';
-for ($i = count($arr4) - 1; $i >= 0; $i--) {
-    echo $arr4[$i] . " ";
-}
-echo '<br>';
+//echo " <br> 4 задание 15 слайда <br>";
+//$arr6 = [];
+//$p = mt_rand(-100, 100);
+//$n = 10;
+//for ($i = 0; $i <= $n; $i++) {
+//    $arr6[$i] = mt_rand(-100, 100);
+//}
+//
+//$min = 100;
+//$max = -100;
+//for ($i = 0; $i < count($arr6); $i++) {
+//    if ($arr6[$i] > 0 and $min > $arr6[$i]) {
+//        $min = $arr6[$i];
+//    }
+//    elseif($arr6[$i] < 0 and $max < $arr6[$i]) {
+//        $max = $arr6[$i];
+//    }
+//}
+//print_r($arr6);
+//echo "<br>";
+////echo $min;
+////echo "<br>";
+//echo "min = " . $min . '<br>' ." max = " . $max;
+//
+////УРОК 7-8
+//echo " <br> 3 задание 22 слайд <br>";
+//$str = "Привет, мир!";
+//$arr1 = mb_str_split($str);
+//print_r($arr1);
+//$sum = 0;
+//for ($i = 0; $i < count($arr1) - 1; $i++ ){
+//    if ($arr1[$i] == 'и')
+//        $sum++;
+//}
+//echo $sum;
 
-echo " <br> 3 задание 15 слайда <br>";
-$arr5 = ['1.png', '2.png', '3.png', '4.png'];
-$d = mt_rand(0, 3);
-echo $arr5[$d];
-echo '<br>';
+//echo " <br> 4 задание 22 слайд <br>";
+//$str = 'html css php';
+//$arr1 = explode(" ",$str);
+//echo '<br>';
+//print_r($arr1[0]);
+//echo '<br>';
+//print_r($arr1[1]);
+//echo '<br>';
+//print_r($arr1[2]);
 
-echo " <br> 4 задание 15 слайда <br>";
-$arr6 = [];
-$p = mt_rand(-100, 100);
-$n = 10;
-for ($i = 0; $i <= $n; $i++) {
-    $arr6[$i] = mt_rand(-100, 100);
-}
-sort($arr6);
-$min = 0;
-$max = 0;
-for ($i = 0; $i < count($arr6) - 1; $i++) {
-    if ($arr6[$i] >= 0 and $min < $arr6[$i]) {
-        $max = max($arr6[$i], $arr6[$i + 1]);
+//echo " <br> 5 задание 22 слайд <br>";
+//$str = 'file.jpeg';
+//$arr1 = explode('.', $str);
+//print_r($arr1);
+//foreach ($arr1 as $item){
+//    if($item == "png"){
+//        echo "да"; break;
+//    }
+//    else echo "нет"; break;
+//}
+
+echo " <br> 8 задание 17 слайд <br>";
+
+$arr1 = [["банан", "ананас", "яблоко"], ["андромеда", "бельтегейзе", "альдебаран"]];
+foreach ($arr1 as $key => $value) {
+    foreach ($value as $item) {
+        $a = mb_str_split($item);
+        if ($a[0] == 'а') {
+            echo $item . '<br>';
+        }
     }
-    else {
-        $min = min($arr6[$i], $arr6[$i + 1]);
+}
+
+echo " <br> 9 задание 17 слайд <br>";
+$arr2 = [[1,2,3],[2,3]];
+$arr2Count = 0;
+foreach ($arr2 as $key => $value){
+    if($arr2Count == 0){
+        $arr2Count = count($arr2);
+        echo "Количество элементов массива:".$arr2Count.'</br>';
+    }
+    foreach($value as $item ){
+        echo "Количество элементов измерения:".count($value).'</br>';
+        break;
     }
 }
-print_r($arr6);
-echo $min;
-echo "<br>";
-echo $max;
